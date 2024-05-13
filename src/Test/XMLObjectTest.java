b37a99077c29882f7c8b910c98a0db93f0efea88
athdeleonardis <athdeleonardis@gmail.com>
2024-05-13 19:32:25 +1000

    Created test for the XMLObject. A very simple one.

---
+++ src/Test/XMLObjectTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+import XML.XMLObject;
+
+public class XMLObjectTest {
+    public static void main(String[] args) {
+        String filename = args[0];
+        new XMLObject("Parent")
+                .addChild("Child1", "Value1")
+                .addChild("Child2", "Value2")
+                .compile(filename);
+    }
+}
